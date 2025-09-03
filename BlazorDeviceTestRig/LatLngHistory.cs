f027273df68954d0f973b456d5a60263f8a02677
Jeff Mathews <jeff32819@gmail.com>
2025-09-03 16:39:43 -0400

    misc

---
+++ BlazorDeviceTestRig/LatLngHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Darnton.Blazor.DeviceInterop.Geolocation;
+
+namespace BlazorDeviceTestRig;
+
+public class LatLngHistory(GeolocationResult ev)
+{
+    public double Latitude { get; } = ev.Position.Coords.Latitude;
+    public double Longitude { get; } = ev.Position.Coords.Longitude;
+    public DateTime Timestamp { get; } = DateTime.UtcNow;
+    public string TimestampString => $"{Timestamp:yyyy/MM/dd HH:mm:ss}";
+}
